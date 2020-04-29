2007d65814f705a37dae3675a92caa0fb7a8a204
Gergő Balogh <geryxyz@gmail.com>
2020-04-29 15:19:54 +0200

    usage of generated lib from Core 3.1 Console application

---
+++ OpenAPISample/OpenAPISampleConsoleClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using OpenAPISampleClientLib;
+
+namespace OpenAPISampleConsoleClient
+{
+    class Program
+    {
+        static void Main(string[ ] args)
+        {
+            Console.WriteLine("This is just a sample Console Client for OpenAPI.");
+            WeatherForecastClient client = new WeatherForecastClient(new System.Net.Http.HttpClient( ));
+            Console.WriteLine($"The client will get forcasts form {client.BaseUrl} server." );
+            var request = client.GetAsync( );
+            request.Wait( );
+            foreach(var forcast in request.Result)
+            {
+                Console.WriteLine(forcast.Summary);
+            }
+        }
+    }
+}
