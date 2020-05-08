ac39856c4e26584532e6d2392e5219a9bf958992
Gergő Balogh <geryxyz@gmail.com>
2020-05-08 10:57:23 +0200

    manually adding generated code to unity and REST call works

---
+++ OpenAPISample3D/Assets/Try.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OpenAPISampleClientLib;
+
+public class Try : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("This is just a sample Unity Client for OpenAPI.");
+        WeatherForecastClient client = new WeatherForecastClient("https://localhost:44390", new System.Net.Http.HttpClient( ));
+        Debug.Log($"The client will get forcasts form {client.BaseUrl} server.");
+        var request = client.GetAsync( );
+        request.Wait( );
+        foreach(var forcast in request.Result)
+        {
+            Debug.Log(forcast.Summary);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
