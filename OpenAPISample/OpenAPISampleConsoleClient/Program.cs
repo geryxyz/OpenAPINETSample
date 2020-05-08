20e1c246f55155100ccef1af498eb55254b1c685
Gergő Balogh <geryxyz@gmail.com>
2020-05-08 17:10:09 +0200

    unity and openapi working with nswag trick to belive that the lib is still Core 3.1

--- OpenAPISample/OpenAPISampleConsoleClient/Program.cs
+++ OpenAPISample/OpenAPISampleConsoleClient/Program.cs
@@ -8,7 +8,7 @@ namespace OpenAPISampleConsoleClient
         static void Main(string[ ] args)
         {
             Console.WriteLine("This is just a sample Console Client for OpenAPI.");
-            WeatherForecastClient client = new WeatherForecastClient(new System.Net.Http.HttpClient( ));
+            WeatherForecastClient client = new WeatherForecastClient("https://localhost:44390", new System.Net.Http.HttpClient( ));
             Console.WriteLine($"The client will get forcasts form {client.BaseUrl} server." );
             var request = client.GetAsync( );
             request.Wait( );
